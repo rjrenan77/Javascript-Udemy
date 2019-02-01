e8385f43422f8e164eebe6dfe9784af16c7f0493
Renan Soares <rjrenan77@gmail.com>
2019-02-01 11:12:45 -0200

    ajax

--- ajax/server.js
+++ ajax/server.js
@@ -7,5 +7,29 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 //app.get("/teste", (req,res) => res.send(new Date));
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, "./upload");
+    },
+    filename: function(req,file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`);
+
+    }
+
+
+});
+
+const upload = multer({storage}).single("arquivo");
+app.post("/upload", (req,res)=>{
+    upload(req, res, err=>{
+        if(err){
+            return res.end("Ocorreu um erro");
+        }
+
+        res.end("concluído");
+    });
+});
 
 app.listen(8080,()=>console.log("Executando.."));
